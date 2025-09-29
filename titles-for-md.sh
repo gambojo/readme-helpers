fb5e67d758c0a4a3505e7979878536d9b970d3a1
Agamov Gamid <agamovga@cloudx.group>
2025-09-29 11:44:22 +0300

    ---

---
+++ titles-for-md.sh
@@ -0,0 +1,50 @@
+#! /usr/bin/env bash
+set -e
+
+readLine() {
+    __fileName__="${1}"
+    sed -n '/^#\{1,3\}[^#]/p' "${__fileName__}"
+}
+
+setTitle() {
+    __line__="${@}"
+    printf '%s\n' "${__line__}" |
+        sed 's/^#* //'
+}
+
+setAnchor() {
+    __title__="${@}"
+    printf '%s\n' "${__title__}" |
+        tr ' ' '-'
+}
+
+setLevel() {
+    __line__="${@}"
+    printf '%s\n' "${__line__}" |
+        grep -o '^#*' |
+        wc -c
+}
+
+setIdent() {
+    __level__="$@"
+    printf '%*s' $((__level__-2)) ''
+}
+
+formatter() {
+    __indent__="${1}"
+    __title__="${2}"
+    __anchor__="${3}"
+    printf '%s\n' "${__indent__}- [${__title__}](#${__anchor__})"
+}
+
+main() {
+    while read -r line; do
+        title=$(setTitle ${line})
+        anchor=$(setAnchor ${title})
+        level=$(setLevel ${line})
+        ident=$(setIdent ${level})
+        formatter "${ident}" "${title}" "${anchor}"
+    done <<< $(readLine "$@")
+}
+
+main "$@"
